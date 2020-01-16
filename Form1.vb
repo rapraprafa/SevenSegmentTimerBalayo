dd90fe3c358b8dd067fefb5a12fd9413fd08b960
rapraprafa <rafaeljohnbalayo@gmail.com>
2020-01-17 07:35:06 +0800

    Functionality Seven Segment 0-2

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class SevenSegmentTimerBalayo
+    Dim Second As Int32
+
+    Private Sub SevenSegmentTimerBalayo_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Timer1.Start()
+
+    End Sub
+End Class
